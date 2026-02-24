0e1516058e2be9edaee1c868d85061f66bc0bc8a
Nazrul Nazir <nazrul6290@gmail.com>
2026-02-24 23:04:34 +0600

    modify btn style

--- script.js
+++ script.js
@@ -140,7 +140,8 @@ let allRejected = [];
 
                         <div>
                             <p class="description">${interveiw.description}</p>
-                            <button class="interview-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-green-600 border border-green-600 hover:text-green-500 hover:border-green-500 mr-4 mt-5">INTERVIEW</button>
+                            <button class="interview-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-green-600 border border-green-600 hover:text-green-500 hover:border-green-500 mr-1 md:mr-4 mt-5">INTERVIEW</button>
+
                             <button class="reject-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-red-500 border border-red-500 hover:border-red-400 hover:text-red-400">REJECTED</button>
                         </div>
                     </div>
@@ -188,7 +189,8 @@ let allRejected = [];
 
                     <div>
                         <p class="description">${rejected.description}</p>
-                        <button class="interview-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-green-600 border border-green-600 hover:text-green-500 hover:border-green-500 mr-4 mt-5">INTERVIEW</button>
+                        <button class="interview-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-green-600 border border-green-600 hover:text-green-500 hover:border-green-500 mr-1 md:mr-4 mt-5">INTERVIEW</button>
+
                         <button class="reject-btn px-5 py-2 rounded-md my-3 cursor-pointer text-sm font-semibold text-red-500 border border-red-500 hover:border-red-400 hover:text-red-400">REJECTED</button>
                     </div>
                 </div>
